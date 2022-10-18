5cd403058678f2371decf99a978dc55ed1db8f54
Denis-92 <d.mozenic@gmail.com>
2022-10-18 13:49:09 +0200

    add component song container

---
+++ prima_versione/component_song.php
@@ -0,0 +1,13 @@
+<div class="song-container">
+
+    <div>
+        <img src="<?= $song['poster'] ?>" alt="<?= $song['title'] ?>" />
+    </div>
+
+    <h2><?= $song['title'] ?></h2>
+
+    <p><?= $song['year'] ?></p>
+
+    <p><?= $song['author'] ?></p>
+    
+</div>
